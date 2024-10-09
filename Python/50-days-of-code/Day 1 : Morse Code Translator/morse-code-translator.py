3c16c4efd05f7c4dd73d6e5aa0a92f6b8c063c5b
anonyme012 <cacaproutfirst@gmail.com>
2024-10-09 21:11:48 +0200

    Finalize Python Script of Day 1

--- Python/50-days-of-code/Day 1 : Morse Code Translator/morse-code-translator.py
+++ Python/50-days-of-code/Day 1 : Morse Code Translator/morse-code-translator.py
@@ -89,13 +89,21 @@ def translate(latin_string) :
     length = len(latin_string)
     count = 0
     morse_string = ""
-    while count >= length - 1 : 
+    while count <= length - 1 : 
         character = morse_code[latin_string[count]]
         morse_string = morse_string + character + " "
         count += 1
-    print(morse_string)
+    return morse_string
 
 # Execution
 while 0 != 1 :
-    output = translate(input("Enter a string to convert to Morse code : "))
+    output = translate(input("Enter a string to convert to Morse code (^C to quit) : "))
     print(output)
+
+# My errors during programming
+# - I wrote >= instead of <= on the while loop (line 92)
+
+# Possibles improvements : 
+# - Handle the case where the user enters an unknown character
+# - Use a "for" loop instead of a "while" loop (I only know "while" now)
+# - Add characters in the dictionnary
